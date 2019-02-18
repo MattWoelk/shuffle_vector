b63760fbd6bef135c34c65241e9d091f6ee97be0
MattWoelk <matt.woelk@gmail.com>
2019-02-18 10:47:58 -0600

    very basic working example with "push"

---
+++ src/lib.rs
@@ -0,0 +1,53 @@
+use rand::prelude::*;
+
+#[derive(Debug)]
+struct ShuffleVector<T> {
+    vector: Vec<T>,
+}
+
+impl<T> ShuffleVector<T> {
+    fn push(&mut self, item: T) {
+        // push the item to the end of the vector
+        self.vector.push(item);
+
+        // swap last item with random item (which could be itself)
+        let highest_index =  self.vector.len() - 1;
+        if highest_index != 0 {
+            self.vector.swap(thread_rng().gen_range(0, highest_index), highest_index);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ShuffleVector;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn create_empty() {
+        let v = ShuffleVector::<u32> { vector: vec!()};
+        println!("{:?}", v);
+    }
+
+    #[test]
+    fn create_one_item() {
+        let mut v = ShuffleVector::<u32> { vector: vec!()};
+        v.push(1);
+        println!("{:?}", v);
+    }
+
+    #[test]
+    fn create_1_to_5() {
+        let mut v = ShuffleVector::<u32> { vector: vec!()};
+        v.push(1);
+        v.push(2);
+        v.push(3);
+        v.push(4);
+        v.push(5);
+        println!("{:?}", v);
+    }
+}
